510ba74995ded102753f71cbd87ba258bd4ceb27
unknown <ariast390@gmail.com>
2023-05-31 21:24:21 +0700

    adding myproject section, project page, and improve UI appearance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./dist/projects/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        primary: "#F9A03F",
-        secondary: "#C0E5C8",
-        third: "#A40E4C",
-        abu: "#4E5340",
-        dark: "#0f172a",
+        primary: "#1B9C85",
+        secondary: "#1b439c",
+        third: "#136d65",
+        white: "#F0F0F0",
+        dark: "#4C4C6D",
       },
       screens: {
         "2xl": "1320px",
